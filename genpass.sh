ffd0326567a0d4f0450ec395a403325a6b631d52
markfazakerley <ops@voyanta.com>
2018-11-22 13:19:44 +0000

    second  commit

--- genpass.sh
+++ genpass.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# 
+# (c) Mark Fazakerley 2018
+# 
+# Generate a few passwords for people. 
+# 
+
 WORDFILE=./words
+
 # seed random from pid
 
 for i in 1 2 3 4 5
@@ -8,7 +15,7 @@ do
 	RANDOM2=$RANDOM
 	RANDOM3=$((RANDOM%900+100))
 
-	# using cat means wc outputs only a number, not number followed by filename
+	# using cat means wc outputs only a number, not a number followed by a filename
 	lines=$(cat $WORDFILE  | wc -l);
 	rnum1=$((RANDOM2*RANDOM2%$lines+1));
 	rnum2=$((RANDOM1*RANDOM1%$lines+1));
